9debd328f7c6520b8942cd511b877848acad3080
Michal Pawlak <mpawlak.it@gmail.com>
2023-03-10 14:47:05 +0100

    install and configure eslint,prettier and react-scroll

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'require-jsdoc': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+}
