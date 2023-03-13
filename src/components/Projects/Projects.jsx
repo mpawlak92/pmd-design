7cdc8ad956ab6a1b824ed923611f9e54002148ee
Michal Pawlak <mpawlak.it@gmail.com>
2023-03-13 21:48:07 +0100

    feat: created projects section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,13 @@
 import * as React from 'react'
-import { Box, Typography } from '@mui/material'
+import projImg1 from '../../img/proj1.jpg'
+import projImg2 from '../../img/proj2.jpg'
+import projImg3 from '../../img/proj3.jpg'
+import projImg4 from '../../img/proj4.jpg'
+import projImg5 from '../../img/proj5.jpg'
+import projImg6 from '../../img/proj6.jpg'
+import projImg7 from '../../img/proj7.jpg'
+import projImg8 from '../../img/proj8.jpg'
+import { Box, ImageList, ImageListItem, Typography } from '@mui/material'
 
 export default function Projects() {
   return (
@@ -9,7 +17,7 @@ export default function Projects() {
           display: 'flex',
           justifyContent: 'center',
           width: '100%',
-          my: { xs: 7, sm: 8 },
+          my: { xs: 8, sm: 9 },
         }}
       >
         <Typography variant="h2">My Projects</Typography>
@@ -22,8 +30,53 @@ export default function Projects() {
           mb: { xs: 7, sm: 8 },
         }}
       >
-        <Typography variant="h2">Content</Typography>
+        <ImageList sx={{ width: '90%' }} variant="woven" cols={4} gap={100}>
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                src={`${item.img}?w=161&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=161&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
       </Box>
     </>
   )
 }
+const itemData = [
+  {
+    img: `${projImg1}`,
+    title: 'Bed',
+  },
+  {
+    img: `${projImg2}`,
+    title: 'Kitchen',
+  },
+  {
+    img: `${projImg3}`,
+    title: 'Sink',
+  },
+  {
+    img: `${projImg4}`,
+    title: 'Books',
+  },
+  {
+    img: `${projImg5}`,
+    title: 'Chairs',
+  },
+  {
+    img: `${projImg6}`,
+    title: 'Candle',
+  },
+  {
+    img: `${projImg7}`,
+    title: 'Laptop',
+  },
+  {
+    img: `${projImg8}`,
+    title: 'Doors',
+  },
+]
