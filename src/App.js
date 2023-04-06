375fc9f77b5637ed9720836cf4949568634e3805
Michal Pawlak <mpawlak.it@gmail.com>
2023-04-06 14:25:46 +0200

    change destination of render menu componnet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import Menu from './components/Menu/Menu'
 import Section from './components/Section/Section'
+import Menu from './components/Menu/Menu'
 function App() {
   return (
-    <div className="App">
-      <Menu />
-      <Section id="#" name="heroImg" />
-      <Section id="projects" name="projects" />
-      <Section id="about" name="about" />
-      <Section id="servicees" name="services" />
-      <Section id="contact" name="contact" />
-    </div>
+    <>
+      <div className="App">
+        <Menu />
+        <Section id="#" name="heroImg" />
+        <Section id="projects" name="projects" />
+        <Section id="about" name="about" />
+        <Section id="servicees" name="services" />
+        <Section id="contact" name="contact" />
+      </div>
+    </>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
+import Routing from './Routing/Routing'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <Routing />
+    </React.StrictMode>
+  </BrowserRouter>
 )
