c3675357316c95b7c6dbd01da70138c4786e18b6
Michal Pawlak <mpawlak.it@gmail.com>
2023-05-26 09:51:47 +0200

    eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,21 @@
-module.exports = {
+export default {
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   overrides: [],
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['react', 'prettier'],
   rules: {
-    'require-jsdoc': 'off',
-    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'error',
+    semi: 1,
+    quotes: 1,
   },
 }
